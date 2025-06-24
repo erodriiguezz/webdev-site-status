9b9dc5b49dd4d5088639e846865a1f39851dc262
Ernesto Rodriguez <erodriiguezz@icloud.com>
2025-06-24 13:38:35 -0400

    added main status page link to section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,13 @@
-const Section = ({ name, children }) => {
+const Section = ({ name, statusPageUrl, children }) => {
   return (
     <section className="border border-gray-200 rounded p-4">
       <h2>{name}</h2>
+
+      {statusPageUrl && (
+        <a className="block mb-6 color-brand cursor-pointer" href={statusPageUrl} target="_blank">
+          Click here to view more vitals.
+        </a>
+      )}
 
       <div>{children}</div>
     </section>
